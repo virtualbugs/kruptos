ce87290e390094305782aa4b9e2479105b7c7e55
_caliskan_ <caliskan2mustafa@gmail.com>
2023-01-01 11:59:12 +0300

    simple encrypt test-2 is added

--- _encrypt_.py
+++ _encrypt_.py
@@ -3,19 +3,37 @@ import io
 value="good"
 encryp=""
 decryp=""
+class EncrypType:
+    ENCRYP = 0
+    DECRYP = 1
+
 
 
 def Test01(crypto_type):
     global encryp
     global decryp
-    if crypto_type == 0:
-        for i in range(len(value)):
+    for i in range(len(value)):
+        if crypto_type == EncrypType.ENCRYP:
             encryp = encryp + chr(ord(value[i])+2) 
-    else:
-        for i in range(len(value)):
+        elif crypto_type == EncrypType.DECRYP:
             decryp = decryp + chr(ord(value[i])-2) 
         
 
-#Test01(1)
+def Test02(crypto_type):
+    global encryp
+    global decryp
+    len_val = len(value)
+    for i in reversed(range(len_val)):
+        if crypto_type == EncrypType.ENCRYP:
+            encryp = encryp + value[i]
+        elif crypto_type == EncrypType.DECRYP:
+            decryp = decryp + value[i] 
+
+
+#Test01(EncrypType.ENCRYP)
 #print("encryp : " , encryp)
 #print("decryp : " , decryp)
+
+Test02(EncrypType.ENCRYP)
+print("encryp : " , encryp)
+print("decryp : " , decryp)
